413e1e1315f79a153bdad8f147758db2a81404dc
karim-c <kcis880@aucklanduni.ac.nz>
2018-08-18 18:18:16 +1200

    update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hitch Building Inspections</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          "Continuous deployment has been set up all changes to master will update this page."
         </p>
       </div>
     );
