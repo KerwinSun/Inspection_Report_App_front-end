774b4525ea0a0d9b62fc6b35e65fb1aaffaaf8a2
chaejj <bchj513@hotmail.com>
2018-08-20 11:12:21 +1200

    API call can be made from react app now

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API = 'http://inspection-report-app-server.azurewebsites.net/api/todo';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    // fetch(API)
+    // fetch(API, { mode: 'no-cors'})
+    fetch(API, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+      .then(res => res.json())
+      .then(res => this.setState({ data: res }))
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +35,9 @@ class App extends Component {
         <p className="App-intro">
           "Continuous deployment has been set up all changes to master will update this page."
         </p>
+        <ul>
+          {this.state.data.map((d, i) => <li key={i}>id: {d.id} name: {d.name} isComplete: {d.isComplete.toString()}</li>)}
+        </ul>
       </div>
     );
   }
