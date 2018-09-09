e644b79b300cb1e5286a35b395431b673438bbeb
whattupjellycup <gman485@aucklanduni.ac.nz>
2018-09-09 15:50:09 +1200

    Take photo (#22)
    * added camera page. Updated package.json for package manager to install react-camera
    
    * added camera switch
    
    * modified package.json and yarn lock to include react-camera
    
    * changed package.son and yarn lock to include react-html5-camera-photo

--- app/src/pages/CameraPage.js
+++ app/src/pages/CameraPage.js
@@ -1,96 +1,21 @@
 import React, { Component } from 'react';
-import Camera from 'react-camera';
-import { Button} from "tabler-react";
-
-const style = {
-  preview: {
-    position: 'relative',
-  },
-  captureContainer: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'center',
-    zIndex: 1,
-    bottom: 0,
-    width: '100%'
-  },
-  captureButton: {
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    height: 56,
-    width: 56,
-    color: '#000',
-    margin: 20
-  },
-  captureImage: {
-    width: '100%',
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+ 
+class CameraPage extends Component {
+  onTakePhoto (dataUri) {
+    // Do stuff with the dataUri photo...
+    console.log('takePhoto');
   }
-};
-
-export default class CameraPage extends Component {
-  constructor(props) {
-    super(props);
-    this.takePicture = this.takePicture.bind(this);
-    this.state = {
-      cameraType : 'back',
-      mirrorMode : false
-    }
-  }
-
-  takePicture() {
-    this.camera.capture()
-    .then(blob => {
-      this.img.src = URL.createObjectURL(blob);
-      this.img.onload = () => { URL.revokeObjectURL(this.src); }
-    })
-  }
-
-  changeCameraType() {
-    console.log("camera switch");
-    if (this.state.cameraType === 'back') {
-      this.setState({
-        cameraType: 'front',
-        mirror: true
-      });
-    } else {
-      this.setState({
-        cameraType: 'back',
-        mirror: false
-      });
-    }
-  }
-
-  render() {
+ 
+  render () {
     return (
-      <div style={style.container}>
-      <Button
-            block={true}
-            color="secondary"
-            onPress={this.changeCameraType.bind(this)}
-        >
-        Switch Camera
-        </Button>
+      <div className="App">
         <Camera
-          type={this.state.cameraType} 
-          mirrorImage={this.state.mirrorMode}
-          style={style.preview}
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-        >
-        <div style={style.captureContainer} onClick={this.takePicture}>
-          <div style={style.captureButton} />
-        </div>
-        </Camera>
-        <img
-          style={style.captureImage}
-          ref={(img) => {
-            this.img = img;
-          }}
-          alt=""
+          onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
         />
       </div>
     );
   }
 }
-
+export default CameraPage;
