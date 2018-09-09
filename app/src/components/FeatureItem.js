dccf0a87528644f846f1f2fc75ce49e36e06ac21
Karim-C <kcis880@aucklanduni.ac.nz>
2018-09-09 15:48:30 +1200

    Integrate the features with the categories page

--- app/src/components/FeatureItem.js
+++ app/src/components/FeatureItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Form } from "tabler-react";
+import { Card, Form, Button, Icon } from "tabler-react";
 
 class FeatureItem extends Component {
   state = {
@@ -20,21 +20,35 @@ class FeatureItem extends Component {
   render() {
     return this.state.featureList.map((dynamicData, i) => (
       <div key={dynamicData.id.toString()}>
-        <Card title={dynamicData.name} isCollapsible isCollapsed={true}>
+        <Card
+          title={dynamicData.name}
+          isCollapsed={true}
+          isClosable
+          isCollapsible
+        >
           <Card.Body>
             <Form.Group label="Rating">
-              <Form.Radio label="Good" name="grade" value="Good" />
+              <Form.Radio name="rating" label="Good" value="1" />
+              <Form.Radio name="rating" label="Will need attention" value="2" />
               <Form.Radio
-                label="Will need attention soon"
-                name="grade"
-                value="Will need attention soon"
-              />
-              <Form.Radio
+                name="rating"
                 label="Need immediate attention"
-                name="grade"
-                value="Need immediate attention"
+                value="3"
+              />
+              <Form.Radio name="rating" label="N/A" value="4" />
+            </Form.Group>
+            <Form.Group label={<Form.Label>Comments</Form.Label>}>
+              <Form.Textarea
+                name="comments"
+                placeholder="Add comment"
+                rows={4}
               />
             </Form.Group>
+            <Button.List align="center">
+              <Button RootComponent="a" href="/camera" color="secondary">
+                <Icon prefix="fe" name="camera" />
+              </Button>
+            </Button.List>
           </Card.Body>
         </Card>
       </div>
