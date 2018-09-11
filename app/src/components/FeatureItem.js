78f639677f97d16ac0d872d60a9177e86e3346a4
Jonathan Soulsby <jonathansoulsby333@gmail.com>
2018-09-11 13:02:55 +1200

    overwrote tabler css to fix drop down icon formatting (#33)

--- app/src/components/FeatureItem.js
+++ app/src/components/FeatureItem.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Form, Button, Icon } from "tabler-react";
-import './Card.css';
+import "./Card.css";
 
 class FeatureItem extends Component {
   constructor(props) {
@@ -9,14 +9,14 @@ class FeatureItem extends Component {
       feature: {},
       index: -1,
       isCollapsed: true,
-      isParentCollapsed: false,
-    }
+      isParentCollapsed: false
+    };
   }
 
   componentDidMount() {
-    this.setState({ 
-      feature: this.props.feature, 
-      index: this.props.index, 
+    this.setState({
+      feature: this.props.feature,
+      index: this.props.index,
       isParentCollapsed: this.props.isParentCollapsed
     });
   }
@@ -27,51 +27,91 @@ class FeatureItem extends Component {
       <div label={this.props.label}>
         <div className="small-card-header">
           {feature.name}
-          <button type="button" onClick={() => {
-            this.setState({ isCollapsed: !isCollapsed }, 
-              () => this.props.updateFeatureState(this.state)
-            );
-          }}>
-            {isCollapsed ? <Icon prefix="fe" name="chevron-down" /> : <Icon prefix="fe" name="chevron-up" />}
+          <button
+            type="button"
+            class="btn btn-secondary"
+            style={{ border: "none", "box-shadow": "none", background: "none" }}
+            onClick={() => {
+              this.setState({ isCollapsed: !isCollapsed }, () =>
+                this.props.updateFeatureState(this.state)
+              );
+            }}
+          >
+            {isCollapsed ? (
+              <Icon prefix="fe" name="chevron-down" />
+            ) : (
+              <Icon prefix="fe" name="chevron-up" />
+            )}
           </button>
         </div>
-        <div className={isCollapsed ? "card-content hidden" : "card-content open"}>
+        <div
+          className={isCollapsed ? "card-content hidden" : "card-content open"}
+        >
           {this.renderFeature()}
         </div>
       </div>
-    )
-  };
+    );
+  }
 
   renderFeature() {
     return (
       <div>
         <Form.Group label="Rating">
-          <Form.Radio onChange={this.radioOnChange.bind(this)} name={"rating"+this.state.feature.name} label="Good" value="1" />
-          <Form.Radio onChange={this.radioOnChange.bind(this)} name={"rating"+this.state.feature.name} label="Will need attention" value="2" />
-          <Form.Radio onChange={this.radioOnChange.bind(this)} name={"rating"+this.state.feature.name} label="Need immediate attention" value="3" />
-          <Form.Radio onChange={this.radioOnChange.bind(this)} name={"rating"+this.state.feature.name} label="N/A" value="4" />
+          <Form.Radio
+            onChange={this.radioOnChange.bind(this)}
+            name={"rating" + this.state.feature.name}
+            label="Good"
+            value="1"
+          />
+          <Form.Radio
+            onChange={this.radioOnChange.bind(this)}
+            name={"rating" + this.state.feature.name}
+            label="Will need attention"
+            value="2"
+          />
+          <Form.Radio
+            onChange={this.radioOnChange.bind(this)}
+            name={"rating" + this.state.feature.name}
+            label="Need immediate attention"
+            value="3"
+          />
+          <Form.Radio
+            onChange={this.radioOnChange.bind(this)}
+            name={"rating" + this.state.feature.name}
+            label="N/A"
+            value="4"
+          />
         </Form.Group>
         <Form.Group label={<Form.Label>Comments</Form.Label>}>
-          <Form.Textarea onChange={this.commentOnChange.bind(this)} name="comments" placeholder="Add comment" rows={4} />
+          <Form.Textarea
+            onChange={this.commentOnChange.bind(this)}
+            name="comments"
+            placeholder="Add comment"
+            rows={4}
+          />
         </Form.Group>
         <Button.List align="center">
           <Button RootComponent="a" href="/camera" color="secondary">
-            <Icon prefix="fe" name="camera" />        
+            <Icon prefix="fe" name="camera" />
           </Button>
         </Button.List>
       </div>
     );
   }
 
-  radioOnChange = (e) => {
-    const feature = Object.assign({}, this.state.feature, { grade: e.target.value });
+  radioOnChange = e => {
+    const feature = Object.assign({}, this.state.feature, {
+      grade: e.target.value
+    });
     this.setState({ feature });
-  }
+  };
 
-  commentOnChange = (e) => {
-    const feature = Object.assign({}, this.state.feature, { comments: e.target.value });
+  commentOnChange = e => {
+    const feature = Object.assign({}, this.state.feature, {
+      comments: e.target.value
+    });
     this.setState({ feature });
-  }
+  };
 }
 
 export default FeatureItem;
