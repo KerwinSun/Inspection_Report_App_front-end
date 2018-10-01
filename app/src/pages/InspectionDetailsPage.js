61f9776010f10e2b2c889650372c266cc7f9bcf1
whattupjellycup <gman485@aucklanduni.ac.nz>
2018-10-01 17:58:49 +1300

    Phone and email validation (#70)
    * added validation for begin inspection input
    
    * added alert for begin inspection validation
    
    * added loader when begin inspection is clicked
    
    * added validation for mobile and home phone
    
    * changed != to !== and removed unneccessry escape characters

--- app/src/pages/InspectionDetailsPage.js
+++ app/src/pages/InspectionDetailsPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Page, Grid, Card, Button, Form } from "tabler-react";
+import { Page, Grid, Card, Button, Form, Alert } from "tabler-react";
 import SiteWrapper from "../SiteWrapper";
 import API from "../api";
 import { jsonHouse, realEstateOptions } from "../config";
+import Loader from "react-loader-spinner";
+import NumberFormat from "react-number-format";
 
 class InspectionDetailsPage extends Component {
   constructor(props) {
@@ -18,16 +20,17 @@ class InspectionDetailsPage extends Component {
       cEmailAddress: "",
       cAddress: "",
       cRealEstate: "",
+      isSubmitClicked: false
     };
   }
 
-  componentDidMount() { 
-    this.setState({ 
+  componentDidMount() {
+    this.setState({
       userId: 1, //Hard coded user id for now.
-      iName: 'Sam Hill', //hard coded inspector for now
+      iName: "Sam Hill", //hard coded inspector for now
       inspectionDate: new Date().toJSON(),
-      cRealEstate: realEstateOptions[0] 
-    })
+      cRealEstate: realEstateOptions[0]
+    });
   }
 
   render() {
@@ -39,21 +42,32 @@ class InspectionDetailsPage extends Component {
               <Card title="Inspection Information">
                 <Card.Body>
                   <Form.Group label="Inspector">
-                    <Form.Input
-                      readOnly
-                      value={this.state.iName}
-                    />
+                    <Form.Input readOnly value={this.state.iName} />
                   </Form.Group>
                   <Form.Group label="Date">
                     <Form.Input
                       readOnly
-                      value={this.state.inspectionDate.substring(0,10).split("-").reverse().join("/")}
+                      value={this.state.inspectionDate
+                        .substring(0, 10)
+                        .split("-")
+                        .reverse()
+                        .join("/")}
                     />
                   </Form.Group>
                   <Form.Group label="Address">
                     <Form.Input
                       placeholder="Address"
-                      onChange={ e => this.setState({ address: e.target.value })}
+                      onChange={e => this.setState({ address: e.target.value })}
+                      feedback={
+                        this.state.address === ""
+                          ? "Please input an address"
+                          : null
+                      }
+                      invalid={
+                        this.state.isSubmitClicked
+                          ? this.state.address === ""
+                          : null
+                      }
                     />
                   </Form.Group>
                 </Card.Body>
@@ -63,41 +77,110 @@ class InspectionDetailsPage extends Component {
               <Card title="Client Details">
                 <Card.Body>
                   <Form.Group label="Summonsed By">
-                    <Form.Input 
+                    <Form.Input
                       placeholder="Name"
-                      onChange={ e => this.setState({ cName: e.target.value })}
+                      onChange={e => this.setState({ cName: e.target.value })}
+                      feedback={
+                        this.state.cName === ""
+                          ? "Please input the client's name"
+                          : null
+                      }
+                      invalid={
+                        this.state.isSubmitClicked
+                          ? this.state.cName === ""
+                          : null
+                      }
                     />
                   </Form.Group>
                   <Form.Group label="Home Phone">
-                    <Form.Input
+                  <NumberFormat
+                      displayType={"input"}
+                      customInput={Form.Input}
                       placeholder="Phone Number"
-                      onChange={ e => this.setState({ cHomePhone: e.target.value })}
+                      onChange={e =>
+                        this.setState({ cHomePhone: e.target.value })
+                      }
+                      feedback={
+                        this.state.cHomePhone === ""
+                          ? "Please input the client's home number"
+                          : null
+                      }
+                      invalid={
+                        this.state.isSubmitClicked
+                          ? this.state.cHomePhone === ""
+                          : null
+                      }
                     />
                   </Form.Group>
                   <Form.Group label="Mobile Phone">
-                    <Form.Input
+                    <NumberFormat
                       placeholder="Mobile Number"
-                      onChange={ e => this.setState({ cMobilePhone: e.target.value })}
+                      displayType={"input"}
+                      customInput={Form.Input}
+                      onChange={e =>
+                        this.setState({ cMobilePhone: e.target.value })
+                      }
+                      feedback={
+                        this.state.cMobilePhone === ""
+                          ? "Please input the client's mobile number"
+                          : null
+                      }
+                      invalid={
+                        this.state.isSubmitClicked
+                          ? this.state.cMobilePhone === ""
+                          : null
+                      }
                     />
                   </Form.Group>
                   <Form.Group label="Address">
                     <Form.Input
                       placeholder="Address"
-                      onChange={ e => this.setState({ cAddress: e.target.value })}
+                      onChange={e =>
+                        this.setState({ cAddress: e.target.value })
+                      }
+                      feedback={
+                        this.state.cAddress === ""
+                          ? "Please input the client's address"
+                          : null
+                      }
+                      invalid={
+                        this.state.isSubmitClicked
+                          ? this.state.cAddress === ""
+                          : null
+                      }
                     />
                   </Form.Group>
                   <Form.Group label="Email Address">
                     <Form.Input
                       placeholder="Email Address"
-                      onChange={ e => this.setState({ cEmailAddress: e.target.value })}
+                      feedback={
+                        this.state.cEmailAddress === ""
+                          ? "Please input a valid email address"
+                          : "Invalid email"
+                      }
+                      invalid={
+                        this.state.isSubmitClicked
+                          ? !this.isEmail(this.state.cEmailAddress)
+                          : !(
+                              this.isEmail(this.state.cEmailAddress) ||
+                              this.state.cEmailAddress === ""
+                            )
+                      }
+                      onBlur={e => {
+                        this.setState({ cEmailAddress: e.target.value });
+                      }}
                     />
                   </Form.Group>
                   <Form.Group>
                     <Form.Label>Real Estate</Form.Label>
-                    <Form.Select onChange={ e => this.setState({ cRealEstate: e.target.value })}>
-                    {realEstateOptions.map((dynamicData, i) => (
-                      <option key={dynamicData}>{dynamicData}</option>
-                    ))}
+                    <Form.Select
+                      onChange={e =>
+                        this.setState({ cRealEstate: e.target.value })
+                      }
+                    >
+                      {realEstateOptions.map((dynamicData, i) => (
+                        <option key={dynamicData}>{dynamicData}</option>
+                      ))}
                     </Form.Select>
                   </Form.Group>
                 </Card.Body>
@@ -105,13 +188,43 @@ class InspectionDetailsPage extends Component {
             </Grid.Col>
           </Grid.Row>
           <Button.List align="center">
+            {this.state.isSubmitClicked ? (
+              !this.isInputValid() ? (
+                <Alert type="danger" icon="alert-triangle">
+                  Invalid information
+                </Alert>
+              ) : (
+                <div className="btn-list text-center">
+                  <Loader
+                    type="ThreeDots"
+                    color="#316CBE"
+                    height={30}
+                    width={30}
+                  />
+                </div>
+              )
+            ) : null}
             <Button
               onClick={() => this.props.history.push("/home")}
               color="secondary"
             >
               Back
             </Button>
-            <Button onClick={() => this.handleClick()} color="secondary">
+            <Button
+              onClick={() => {
+                this.setState(
+                  {
+                    isSubmitClicked: true
+                  },
+                  () => {
+                    if (this.isInputValid()) {
+                      this.handleClick();
+                    }
+                  }
+                );
+              }}
+              color="secondary"
+            >
               Begin Inspection
             </Button>
           </Button.List>
@@ -119,18 +232,33 @@ class InspectionDetailsPage extends Component {
       </SiteWrapper>
     );
   }
+  isInputValid = () => {
+    return (
+      this.isEmail(this.state.cEmailAddress) &&
+      this.state.cEmailAddress !== "" &&
+      this.state.cAddress !== "" &&
+      this.state.cName !== "" &&
+      this.state.cHomePhone !== "" &&
+      this.state.cMobilePhone !== "" &&
+      this.state.address !== ""
+    );
+  };
+  isEmail = email => {
+    var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+  };
 
   handleClick = () => {
-    const { 
-      userId, 
-      inspectionDate, 
-      address, 
-      cName, 
-      cHomePhone, 
-      cMobilePhone, 
-      cEmailAddress, 
-      cAddress, 
-      cRealEstate 
+    const {
+      userId,
+      inspectionDate,
+      address,
+      cName,
+      cHomePhone,
+      cMobilePhone,
+      cEmailAddress,
+      cAddress,
+      cRealEstate
     } = this.state;
 
     var client = {};
@@ -143,18 +271,18 @@ class InspectionDetailsPage extends Component {
 
     var json = jsonHouse;
     var userObject = {
-      "UserId": userId, 
+      UserId: userId
     };
     json.inspectedBy = [userObject];
     json.inspectionDate = inspectionDate;
     json.address = address;
-//    json.summonsedBy = client;
+    //    json.summonsedBy = client;
 
-  API.postHouse(json)
-    .then(id => {
-      this.props.history.push("/inspect/" + id);
-    })
-    .catch(error => {})
+    API.postHouse(json)
+      .then(id => {
+        this.props.history.push("/inspect/" + id);
+      })
+      .catch(error => {});
   };
 }
 
