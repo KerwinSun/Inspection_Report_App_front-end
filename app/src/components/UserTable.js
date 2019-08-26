a3fc1c25dea1753bf8062b4dff7d364cb2e8be24
Youngsun An <youngsun574@gmail.com>
2019-08-26 13:25:34 +1200

    added shitty modal

--- app/src/components/UserTable.js
+++ app/src/components/UserTable.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React from "react";
 import { Table, Button } from "tabler-react";
+import Modal from "react-bootstrap/Modal";
 
-//User card can be stateless (functional component)
-const UserTable = ({ users }) => {
+class UserTable extends React.Component {
+  render() {
+    //User card can be stateless (functional component)
     return (
+      <div>
         <Table>
-            <Table.Header>
-                <Table.ColHeader alignContent={"center"}>ID</Table.ColHeader>
-                <Table.ColHeader alignContent={"center"}>First Name</Table.ColHeader>
-                <Table.ColHeader alignContent={"center"}>Last Name</Table.ColHeader>
-                <Table.ColHeader alignContent={"center"}>Action</Table.ColHeader>
-            </Table.Header>
-            <Table.Body>
-                {users.map(user => (
-                    <Table.Row key={user.id} >
-                        <Table.Col alignContent={"center"}>
-                            {user.id}
-                        </Table.Col>
-                        <Table.Col alignContent={"center"}>
-                            {user.firstName}
-                        </Table.Col>
-                        <Table.Col alignContent={"center"}>
-                            {user.lastName}
-                        </Table.Col>
-                        <Table.Col alignContent={"center"}>
-                            <Button variant="primary">
-                                Edit</Button>
-                        </Table.Col>
-                    </Table.Row>
-                ))}
-
-            </Table.Body>
+          <Table.Header>
+            <Table.ColHeader alignContent={"center"}>ID</Table.ColHeader>
+            <Table.ColHeader alignContent={"center"}>
+              First Name
+            </Table.ColHeader>
+            <Table.ColHeader alignContent={"center"}>Last Name</Table.ColHeader>
+            <Table.ColHeader alignContent={"center"}>Action</Table.ColHeader>
+          </Table.Header>
+          <Table.Body>
+            {this.props.users.map(user => (
+              <Table.Row key={user.id}>
+                <Table.Col alignContent={"center"}>{user.id}</Table.Col>
+                <Table.Col alignContent={"center"}>{user.firstName}</Table.Col>
+                <Table.Col alignContent={"center"}>{user.lastName}</Table.Col>
+                <Table.Col alignContent={"center"}>
+                  <Button variant="primary">Edit</Button>
+                </Table.Col>
+              </Table.Row>
+            ))}
+          </Table.Body>
         </Table>
-
+        <Modal show={true}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            <Button variant="primary">Save Changes</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
+  }
 }
 
 export default UserTable;
