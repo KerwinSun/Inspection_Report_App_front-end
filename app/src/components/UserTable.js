fea34eba4c4a2d1e285db77a6ac9239ea6559706
Youngsun An <youngsun574@gmail.com>
2019-08-28 22:32:27 +1200

    fixed bug where when there were multiple users, when you clicked edit, it would only let you edit the last user

--- app/src/components/UserTable.js
+++ app/src/components/UserTable.js
@@ -6,7 +6,8 @@ class UserTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false
+      showModal: false,
+      id: null
     };
     this.togggleShowModal = this.togggleShowModal.bind(this);
   }
@@ -17,44 +18,55 @@ class UserTable extends React.Component {
     }));
   };
 
+  editButtonClicked(buttonId) {
+    this.setState({
+      showModal: true,
+      id: buttonId
+    });
+  }
+
   render() {
     //User card can be stateless (functional component)
     return (
       <div>
         <Table>
           <Table.Header>
-            <Table.ColHeader alignContent={"center"}>First Name</Table.ColHeader>
+            <Table.ColHeader alignContent={"center"}>
+              First Name
+            </Table.ColHeader>
             <Table.ColHeader alignContent={"center"}>Last Name</Table.ColHeader>
             <Table.ColHeader alignContent={"center"}>Email</Table.ColHeader>
             <Table.ColHeader alignContent={"center"}>Phone</Table.ColHeader>
-            <Table.ColHeader alignContent={"center"}>AccountType</Table.ColHeader>
+            <Table.ColHeader alignContent={"center"}>
+              AccountType
+            </Table.ColHeader>
             <Table.ColHeader alignContent={"center"}>Action</Table.ColHeader>
           </Table.Header>
           <Table.Body>
             {this.props.users.map(user => (
               <Table.Row key={user.firstName}>
-                  <Table.Col alignContent={"center"}>
-                  {user.firstName}
-                </Table.Col>
-                <Table.Col alignContent={"center"}>
-                  {user.lastName}
-                </Table.Col>
-                <Table.Col alignContent={"center"}>
-                  {user.email}
-                </Table.Col>
-                <Table.Col alignContent={"center"}>
-                  {user.phone}
-                </Table.Col>
+                <Table.Col alignContent={"center"}>{user.firstName}</Table.Col>
+                <Table.Col alignContent={"center"}>{user.lastName}</Table.Col>
+                <Table.Col alignContent={"center"}>{user.email}</Table.Col>
+                <Table.Col alignContent={"center"}>{user.phone}</Table.Col>
                 <Table.Col alignContent={"center"}>
                   {user.accountType}
                 </Table.Col>
                 <Table.Col alignContent={"center"}>
-                  <Button variant="primary" onClick={this.togggleShowModal}>
+                  <Button
+                    variant="primary"
+                    onClick={() => this.editButtonClicked(user.id)}
+                    id={user.id}
+                  >
                     Edit
                   </Button>
                 </Table.Col>
                 {this.state.showModal ? (
-                 <UserEditModal togggleShowModal={this.togggleShowModal} user = {user}/>
+                  <UserEditModal
+                    togggleShowModal={this.togggleShowModal}
+                    user={this.props.users.find(x => x.id === this.state.id)}
+                    reload={this.props.reload}
+                  />
                 ) : null}
               </Table.Row>
             ))}
