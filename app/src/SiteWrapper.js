12a141b45251c7d01f431a509025b8bf6f57bdca
Nikhil Donthireddy <nikhilreddy24@hotmail.com>
2019-09-18 19:15:55 +1200

    Modified SiteWrapper so that it acts appropriately for the given user type

--- app/src/SiteWrapper.js
+++ app/src/SiteWrapper.js
@@ -3,8 +3,9 @@ import { NavLink } from "react-router-dom";
 import { Site } from "tabler-react";
 import header from "./resources/logo_with_text.png";
 import profileImage from "./resources/stock_headshot.png";
+import store from "store";
 
-const navBarItems = [
+const adminNavBarItems = [
   { value: "Home", to: "/home", icon: "home", LinkComponent: NavLink },
   {
     value: "New Inspection",
@@ -20,27 +21,47 @@ const navBarItems = [
   }
 ];
 
-const accountDropdownProps = {
-  avatarURL: profileImage,
-  name: "Jake Miller",
-  description: "Administrator",
-  options: [
-    { icon: "user", value: "Profile", to: "/profile" },
-    { icon: "log-out", value: "Sign out", to: "/logout" }
-  ]
-};
+const navBarItems = [
+  { value: "Home", to: "/home", icon: "home", LinkComponent: NavLink },
+  {
+    value: "New Inspection",
+    to: "/new-inspection",
+    icon: "check-square",
+    LinkComponent: NavLink
+  }
+];
+
 
 class SiteWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        user : store.get("user"),
+        accountDropdownProps :  {
+          avatarURL: profileImage,
+          name: store.get("user").firstName + " " + store.get("user").lastName,
+          description: store.get("user").accountType,
+          options: [
+            { icon: "user", value: "Profile", to: "/profile" },
+            { icon: "log-out", value: "Sign out", to: "/logout" }
+          ] 
+        }
+    };
+  }
+  
   render() {
+    const isAdminMode = store.get("user").accountType === 'Admin';
     return (
-      <Site.Wrapper
+      <div>
+        {  isAdminMode 
+        ?  <Site.Wrapper
         headerProps={{
           href: "/",
           alt: "Hitch Building Inspections",
           imageURL: header,
-          accountDropdown: accountDropdownProps
+          accountDropdown: this.state.accountDropdownProps
         }}
-        navProps={{ itemsObjects: navBarItems }}
+        navProps={{ itemsObjects: adminNavBarItems }}
         footerProps={
           {
             /* ADD FOOTER HERE
@@ -51,6 +72,28 @@ class SiteWrapper extends Component {
       >
         {this.props.children}
       </Site.Wrapper>
+      :  <Site.Wrapper
+      headerProps={{
+        href: "/",
+        alt: "Hitch Building Inspections",
+        imageURL: header,
+        accountDropdown: this.state.accountDropdownProps
+      }}
+      navProps={{ itemsObjects: navBarItems }}
+      footerProps={
+        {
+          /* ADD FOOTER HERE
+        copyright: <React.Fragment>Do we want a footer?</React.Fragment>
+        */
+        }
+      }
+    >
+      {this.props.children}
+    </Site.Wrapper>
+
+      }
+      </div>
+     
     );
   }
 }
