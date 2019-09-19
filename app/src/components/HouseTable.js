e89cfcb3fc89cfe05566230c9a8562567b14b509
Woojin Kang <wkang9497@gmail.com>
2019-09-19 21:18:27 +1200

    Made custom styling to align all contents of inspection board

--- app/src/components/HouseTable.js
+++ app/src/components/HouseTable.js
@@ -5,21 +5,19 @@ import ReportSharing from "./ReportSharing"
 //House card can be stateless (functional component)
 
 
-const HouseTable = ({ houses, accountType}) => {
+const HouseTable = ({ houses, accountType }) => {
     return (
         <Table cards={true} responsive={true} className="table-vcenter">
             <Table.Body>
                 {houses.map(house => (
                     <Table.Row key={house.id}>
-                        <Table.Col alignContent={"center"}>
-                            {house.inspectionDate.substring(0,10).split("-").reverse().join("/")}
-                        </Table.Col>
-                        <Table.Col alignContent={"center"}>
-                            {accountType ==="Client"? <a>{house.address}</a>:<a href={"/inspect/" + house.id}>{house.address}</a>}
-                        </Table.Col>
-                        <Table.Col alignContent={"center"}>
-                            <ReportSharing house={house}></ReportSharing>   
-                        </Table.Col>
+                        <div className="inspection-board">
+                            {house.inspectionDate.substring(0, 10).split("-").reverse().join("/")}
+                     
+                            {accountType === "Client" ? <a>{house.address}</a> : <a href={"/inspect/" + house.id}>{house.address}</a>}
+                        
+                            <ReportSharing house={house}></ReportSharing>
+                        </div>
                     </Table.Row>
                 ))}
             </Table.Body>
