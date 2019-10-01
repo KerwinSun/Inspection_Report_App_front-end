3229cd8577cd1638f20eae62186e2f4dd7b77609
daedae-k <josephkim96.nz@gmail.com>
2019-10-01 17:08:16 +1300

    Merge pull request #12 from KerwinSun/bug/checkEmail --fix

--- app/src/api.js
+++ app/src/api.js
@@ -79,7 +79,8 @@ export default {
     };
     return axiosInstance(payload).then(response => {
       return response.data;
-    })    
+    });
+  },    
 
   changePassword(json) {
     let payload = {
