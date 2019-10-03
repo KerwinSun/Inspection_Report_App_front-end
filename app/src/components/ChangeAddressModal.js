d3cd42546bcbfa92e0f8b7e736ad128543e03cfa
William Chao <wcha609@aucklanduni.ac.nz>
2019-10-03 17:48:52 +1300

    Add change address for clients in pending inspection

---
+++ app/src/components/ChangeAddressModal.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import { Card, Button, Form } from "tabler-react";
+import API from "../api";
+
+class ChangeAddressModal extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.saveChanges = this.saveChanges.bind(this);
+
+    this.state = {
+      show: true,
+      address: "",
+      isSubmitClicked: false
+    };
+  }
+
+  saveChanges() {
+    if (this.state.address !== "") {
+      let houseData = this.props.house;
+      houseData.address = this.state.address;
+      API.postHouse(houseData)
+        .then(res => {
+          this.props.componentDidMount();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      this.setState({ show: false });
+      this.props.toggleModal();
+    }
+  }
+
+  render() {
+    return (
+      <Modal show={this.state.show} onHide={this.props.toggleModal}>
+        <Modal.Header>
+          <Modal.Title>Change Address</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card>
+            <Card.Body>
+              <Form.Group label="Address">
+                <Form.Input
+                    type='input'
+                    placeholder={this.props.house.address}
+                    onChange={e => this.setState({address: e.target.value})}
+                    feedback={
+                        this.state.address === ""
+                        ? "Please input an address"
+                        : null
+                    }
+                    invalid={
+                        this.state.isSubmitClicked
+                            ? this.state.address === ""
+                            : false
+                    }
+                />
+              </Form.Group>
+            </Card.Body>
+          </Card>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.props.toggleModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={this.saveChanges}>
+            Change Address
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default ChangeAddressModal;
--- app/src/components/HouseTable.js
+++ app/src/components/HouseTable.js
@@ -1,27 +1,80 @@
 import React from 'react';
-import { Table } from "tabler-react";
-import ReportSharing from "./ReportSharing"
+import { Table, Button } from "tabler-react";
+import ReportSharing from "./ReportSharing";
+import ChangeAddressModal from "./ChangeAddressModal";
 
-const HouseTable = ({ houses, accountType, houseState }) => {
-    return (
-        <Table cards={true} responsive={true} className="table-vcenter">
+class HouseTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false,
+            houseData: null
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal = () => {
+        this.setState(prevState => ({
+            showModal: !prevState.showModal
+        }));
+    };
+
+    addressClicked(house) {
+        this.setState({
+            showModal: true,
+            houseData: house
+        })
+    }
+
+    checkAccountType(accountType, house, houseState) {
+        let res = [];
+        if (accountType !== "Client") {
+            // Inspectors should be able to edit anywhere and download in progress & completed
+            res.push(<a href={"/inspect/" + house.id}>{house.address}</a>);
+            if (houseState === "completed" || houseState === "inprogress") {
+                res.push(<ReportSharing house={house}></ReportSharing>);
+            }
+        } else if (accountType === "Client") {
+            res.push(<a>{house.address}</a>);
+            if (houseState === "pending") {
+                //Client should be able to edit the address here
+                res.push(
+                <Button
+                    variant="primary"
+                    onClick={() => this.addressClicked(house)}    
+                >
+                    Change address
+                </Button>)
+            } else if (houseState === "completed") {
+                //Client should be able to download report
+                res.push(<ReportSharing house={house}></ReportSharing>)
+            } 
+        }
+        return res;
+    }
+
+    render() {
+        return (
+            <Table cards={true} responsive={true} className="table-vcenter">
             <Table.Body>
-                {houses.map(house => (
+                {this.props.houses.map(house => (
                     <Table.Row key={house.id}>
                         <div className="inspection-board">
                             {house.inspectionDate.substring(0, 10).split("-").reverse().join("/")}
-
-                            {accountType !== "Client" ? <a href={"/inspect/" + house.id}>{house.address}</a> : ""}
-                            {accountType === "Client" && houseState === "pending"? <a href={"/new-inspection"}>{house.address}</a> : ""}
-                            {accountType === "Client" && houseState !== "pending"? <a>{house.address}</a> : ""}
-                            
-                            {houseState === "completed" || accountType !== "Client" && houseState !== "pending" ? <ReportSharing house={house}></ReportSharing> : ""}
+                            {this.checkAccountType(this.props.accountType, house, this.props.houseState)}
+                            {this.state.showModal ? (
+                                <ChangeAddressModal 
+                                    toggleModal={this.toggleModal}
+                                    house={this.state.houseData}
+                                    componentDidMount={this.props.componentDidMount}
+                                />
+                            ) : null}
                         </div>
                     </Table.Row>
                 ))}
             </Table.Body>
         </Table>
-    );
-}
+        );
+    }
 
-export default HouseTable;
+} export default HouseTable;
